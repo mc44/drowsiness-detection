889eb4ffe55907c19ff4d2e40ad394a09dddefe5
mc44 <marcfajardo14@gmail.com>
2023-03-15 20:38:54 +0800

    client server message test

---
+++ tclientser/server.py
@@ -0,0 +1,19 @@
+import socket
+
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 1234
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(5)
+
+    while True:
+        client, address = server.accept()
+        print(f"Connection Established - {address[0]}:{address}")
+
+        string = client.recv(1024)
+        string = string.decode("utf-8")
+        print(string)
+
+        client.close()
